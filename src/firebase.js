790569fd442e9fcaee19ff711dc741bf6a1c1bca
Ashot Sargsyan <ashotsarkisyan@bk.ru>
2025-10-27 22:51:53 +0400

    **

--- src/firebase.js
+++ src/firebase.js
@@ -16,7 +16,7 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5IkVLcxBMyBZW33EmS77aHJWrVUfd9l8",
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: "free-zone-5e0da.firebaseapp.com",
   projectId: "free-zone-5e0da",
   storageBucket: "free-zone-5e0da.firebasestorage.app",
